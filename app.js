5fbe5d1c531c55d722d72d148e045f72924836c4
Carlos Delao Martinez <delao.carlos@gmail.com>
2019-09-30 19:46:37 -0700

    got the paths working

--- app.js
+++ app.js
@@ -6,10 +6,78 @@
   // move the level around the character
   // generate gaps the rule is that the screen must be filled but the gaps 
   // will be generated randomly with a set min width
+document.addEventListener('DOMContentLoaded', () => {
+  const gameCanvas = document.querySelector('canvas');
+  gameCanvas.width = Game.X_DIMS;
+  gameCanvas.height = Game.Y_DIMS;
+
+  const context = gameCanvas.getContext('2d');
+  const game = new Game();
+  new GameView(game, context);
+})
+
+class GameView {
+  constructor(game, ctx){
+    this.game = game;
+    this.ctx = ctx
+    this.loadLevel(ctx)
+  }
+
+  loadLevel(ctx){
+    console.log('loading level')
+    this.game.colorBackground(ctx);
+    this.game.loadPaths(ctx)
+  }
+}
 
 class Game {
-
+  static Y_DIMS = 600;
+  static X_DIMS = 1000;
+  constructor(){
+    this.width = 40;
+    this.gameLength = this.generateLengthPojos(10)
+  }
+  colorBackground(ctx){
+    ctx.fillStyle = "#4CD8FA";
+    ctx.fillRect(0, 0, Game.X_DIMS, Game.Y_DIMS);
+  }
+  loadPaths(ctx){
+    this.paths = this.generateLengthPojos(200);
+    this.level = new Level(this.paths[0], this.paths[1], this.paths[2],ctx)
+  }
+  // want to be able to make pojos of a given length 
+  // can be used for difficulty
+  generateLengthPojos(minLengthRequirement){
+    const lengths = [{},{},{}]
+    for(let i = 0; i < Game.X_DIMS; i++){
+      let chosenPojo = Math.round(Math.random() * 2)
+      for(let j = 0; j < minLengthRequirement && i < Game.X_DIMS; j++){
+        switch(chosenPojo){
+          case 0:
+            lengths[0][i] = true;
+            lengths[1][i] = false;
+            lengths[2][i] = false; 
+            break;
+          case 1: 
+            lengths[0][i] = false;
+            lengths[1][i] = true;
+            lengths[2][i] = false; 
+            break;
+          case 2:
+            lengths[0][i] = false;
+            lengths[1][i] = false;
+            lengths[2][i] = true; 
+            break;
+        }
+        i++
+      }
+      console.log(lengths);
+    }
+    return lengths;
+  }
 }
+let game = new Game()
+window.game = game;
 
 class Player {
   constructor(){
@@ -18,42 +86,74 @@ class Player {
 
 }
 
-class object {
 
+
+class PathTile {
+  constructor(ctx){
+    this.width = 10;
+    this.height = 10;  
+  }
+  
 }
 
+//hand objects representing the lengths
 class Level {
-  constructor(fgToRender, mgToRender, bgToRender){
-    this.foreground = new Foreground(fgToRender, false);
-    this.middleground = new Middleground(mgToRender, true);
-    this.background = new Background(bgToRender, false);
+  constructor(fgToRender, mgToRender, bgToRender, ctx){
+    this.foreground = new Foreground(fgToRender, false, ctx);
+    this.middleground = new Middleground(mgToRender, true, ctx);
+    this.background = new Background(bgToRender, false, ctx);
   }
 }
 
 class Ground {
-  constructor(groundToRender, povToggled){
+  constructor(groundToRender, povToggled, ctx){
     this.groundArea = (groundToRender);
     this.povToggled = povToggled;
+    this.ctx = ctx;
+  }
+
+  drawRoad(ctx, location, color, lengthsObj){
+    
+    console.log(lengthsObj)
+    for (let x = 0; x < Game.X_DIMS; x ++){
+      if(lengthsObj[x]) {
+        ctx.fillStyle = color
+        ctx.fillRect( x, location, 10, 20)
+      }
+    }
   }
 }
 
-class Foreground extends Ground {
-  constructor(groundToRender, povToggled){
-    super(groundToRender, povToggled)
+
+class Background extends Ground {
+  constructor(groundToRender, povToggled, ctx) {
+    super(groundToRender, povToggled, ctx)
+    this.color = '#EDC9AF'
+    this.roadRow = 300;
+    this.drawRoad(ctx, this.roadRow, this.color, groundToRender)
   }
 }
 
 class Middleground extends Ground {
-  constructor(groundToRender, povToggled) {
-    super(groundToRender, povToggled)
+  constructor(groundToRender, povToggled, ctx) {
+    super(groundToRender, povToggled, ctx)
+    this.color = '#565257';
+    this.roadRow = 400;
+    this.drawRoad(ctx, this.roadRow, this.color, groundToRender)
+
   }
 }
 
-class Background extends Ground {
-  constructor(groundToRender, povToggled) {
+class Foreground extends Ground {
+  constructor(groundToRender, povToggled, ctx) {
     super(groundToRender, povToggled)
+    this.color = '#7cfc00';
+    this.roadRow = 500
+    this.drawRoad(ctx, this.roadRow, this.color, groundToRender)
+
   }
 }
+
 
 class Grid {
   constructor(gridWidth, gridHeight){
@@ -77,9 +177,12 @@ class Grid {
   }
   
 }
-let grid = new Grid(145, 82)
+// let grid = new Grid(70, 40)
 // needs to handle the change between FG <-> MG <-> BG
 // ** show an animation change when transitioning. 
 class POVChange {
 
 }
+
+// 
+// [].slice.call(document.querySelectorAll('.row-30')).forEach(el => el.classList.add('green'))
